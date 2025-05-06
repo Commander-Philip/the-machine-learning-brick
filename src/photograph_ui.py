2cd089156a0faad4e1243ecd6d8b2884a848a2b5
Philip Fritzen <Philip.Fritzen@web.de>
2025-05-06 20:12:07 +0200

    Streaming Klasse eingebaut, Client closed connection implementiert

--- src/photograph_ui.py
+++ src/photograph_ui.py
@@ -18,15 +18,65 @@ from pyb import Timer
 import micropython
 micropython.alloc_emergency_exception_buf(128)
 
+class ClientClosedStreamException(Exception):
+    message = "Client closed the Stream!"
+    def __init__(self):
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+class MjpegStream:
+    def __init__(self, client: socket.socket):
+        self.streamingClient: sock.sock
+        self.startStream()
+
+    def startStream(self):
+        self.streamingClient = client
+
+        # Send multipart header
+        self.streamingClient.sendall(
+            "HTTP/1.1 200 OK\r\n"
+            "Server: OpenMV\r\n"
+            "Content-Type: multipart/x-mixed-replace;boundary=openmv\r\n"
+            "Cache-Control: no-cache\r\n"
+            "Pragma: no-cache\r\n\r\n"
+        )
+
+    def runFrame(self):
+        # Stream images
+        # NOTE: Disable IDE preview to increase streaming FPS.
+        framerateClock.tick()  # Track elapsed milliseconds between snapshots().
+        frame = sensor.snapshot()
+        cframe = frame.to_jpeg(quality=35, copy=True)
+        header = (
+            "\r\n--openmv\r\n"
+            "Content-Type: image/jpeg\r\n"
+            "Content-Length:" + str(cframe.size()) + "\r\n\r\n"
+        )
+        try:
+            self.streamingClient.sendall(header)
+            self.streamingClient.sendall(cframe)
+        except OSError as e:
+            if(e.errno == 32):
+                print("Streaming client closed connection!")
+                self.streamingClient.close()
+                raise ClientClosedStreamException()
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            print(f"Framerate: {framerateClock.fps()}")
+
+
 SSID = "switch2"  # Network SSID
 KEY = "hellebarde"  # Network key
 HOST = ""  # Use first available interface
 PORT = 8080  # Arbitrary non-privileged port
-handleOneConnectionInNextIteration = False
+handleOneConnectionOnNextIteration = False
 command = ""
 client: socket.socket = None
 addr = 0
-clientCurrentlyStreaming: socket.socket = None
+stream: MjpegStream = None
 debug = True
 
 print(f"Frequenz {machine.freq()}")
@@ -37,13 +87,15 @@ sensor.set_framesize(sensor.QVGA)
 sensor.set_pixformat(sensor.RGB565)
 
 # FPS clock
-clock = time.clock()
+framerateClock = time.clock()
+wifiHandlingClock = time.clock()
 
 # Init wlan module and connect to network
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(SSID, KEY)
 
+print(f"Wlan.isconnected:  {wlan.isconnected()}")
 while not wlan.isconnected():
     print('Trying to connect to "{:s}"...'.format(SSID))
     time.sleep_ms(1000)
@@ -64,7 +116,7 @@ print("Socket bound and listening")
 # Set server socket to blocking
 sock.setblocking(False)
 
-def sendWebsite(client: socket.socket) :
+def sendWebsite(client: socket.socket):
     client.sendall(
         "HTTP/1.1 200 OK\r\n"
         "Server: OpenMV\r\n"
@@ -98,53 +150,23 @@ def takePictures(client: socket.socket):
     client.sendall("HTTP/1.1 200 OK\r\n")
     client.close()
 
-
-def startStream(client: socket.socket):
-    global clientCurrentlyStreaming
-    clientCurrentlyStreaming = client
-
-    # Send multipart header
-    clientCurrentlyStreaming.sendall(
-        "HTTP/1.1 200 OK\r\n"
-        "Server: OpenMV\r\n"
-        "Content-Type: multipart/x-mixed-replace;boundary=openmv\r\n"
-        "Cache-Control: no-cache\r\n"
-        "Pragma: no-cache\r\n\r\n"
-    )
-
-
-def runNextStreamIteration(client: socket.socket):
-    # Stream images
-    # NOTE: Disable IDE preview to increase streaming FPS.
-    clock.tick()  # Track elapsed milliseconds between snapshots().
-    frame = sensor.snapshot()
-    cframe = frame.to_jpeg(quality=35, copy=True)
-    header = (
-        "\r\n--openmv\r\n"
-        "Content-Type: image/jpeg\r\n"
-        "Content-Length:" + str(cframe.size()) + "\r\n\r\n"
-    )
-    try:
-        client.sendall(header)
-        client.sendall(cframe)
-    except Exception as e:
-        print(f"Error: {e}")
-
-    print(f"Framerate: {clock.fps()}")
-
 def handleOneRequest():
+    wifiHandlingClock.tick()
     try:
         global command
         global client
         global addr
         global sock
 
-        print("Checking for waiting connections...")
+        # print("Checking for waiting connections...")
         client, addr = sock.accept()
         print("Connected to " + addr[0] + ":" + str(addr[1]))
 
         # Read request from client
         data = client.recv(256)
+        if(not data):
+            print("client closed the connection!")
+            client.close()
         # Should parse client request here
         print("data from client: {}".format(data))
 
@@ -155,6 +177,8 @@ def handleOneRequest():
             if(debug):
                 print("Webseite angefordert")
             command = "SEND_WEBSITE"
+        elif path == "/favicon.ico":
+            client.sendall("HTTP/1.1 404 Bad Request\r\n")
         elif path == "/picture":
             if(debug):
                 print("Nehme Bilder auf...")
@@ -168,17 +192,22 @@ def handleOneRequest():
     except OSError as e:
         if(e.errno != 11):
             print(f"Fehler in Socket Handling: {e}, type {type(e)}")
+    except Exception as e:
+        print(f"Fehler in handleOneRequest: {e}")
+    finally:
+        print(f"Wifi Handler took {wifiHandlingClock.avg()} milliseconds")
 
 
 def runBackgroundLoop(s):
-    global clientCurrentlyStreaming
-    global handleOneConnectionInNextIteration
+    global stream
+    global handleOneConnectionOnNextIteration
     global command
     global client
+    global addr
 
-    if(handleOneConnectionInNextIteration):
+    if(handleOneConnectionOnNextIteration):
         handleOneRequest()
-        handleOneConnectionInNextIteration = False
+        handleOneConnectionOnNextIteration = False
 
     if(not client is None):
         if command == "SEND_WEBSITE":
@@ -187,32 +216,42 @@ def runBackgroundLoop(s):
         elif command == "TAKE_PICTURES":
             takePictures(client)
 
-        elif command == "START_STREAM" and clientCurrentlyStreaming is None:
+        elif command == "START_STREAM" and stream is None:
             if(debug):
                 print("Starte Stream...")
-            startStream(client)
-        elif command == "START_STREAM" and not clientCurrentlyStreaming is None:
+            stream = MjpegStream(client)
+            client = None # Clear current reference so nobody closes the streaming client´
+
+        elif command == "START_STREAM" and not stream is None:
             if(debug):
                 print("Bereits ein User am Streamen")
             client.sendall("HTTP/1.1 503 Service Unavailable\r\n")
-        elif not client is None:
+        elif command == "":
+            pass
+        else:
             if(debug):
-                print("Command not found")
+                print(f"Command: {command} not found")
             client.sendall("HTTP/1.1 404 Not Found\r\n")
 
-    if(not clientCurrentlyStreaming is None):
+    if(not stream is None):
         if(debug):
             print("continue streaming")
-        runNextStreamIteration(clientCurrentlyStreaming)
+        try:
+            stream.runFrame()
+        except ClientClosedStreamException as e:
+            stream = None
+            print(e)
 
     command = ""
-    if(not client is None and clientCurrentlyStreaming != client):
+    if(not client is None):
+        if(debug):
+            print(f"closing connection to client: {addr}")
         client.close()
         client = None
 
 def oneSecondInterrupt(timer):
-    global handleOneConnectionInNextIteration
-    handleOneConnectionInNextIteration = True
+    global handleOneConnectionOnNextIteration
+    handleOneConnectionOnNextIteration = True
 
 # setup timer interrupt
 try:
@@ -227,4 +266,3 @@ while True:
         command = ""
         print("socket error: ", e)
         # sys.print_exception(e)
-
