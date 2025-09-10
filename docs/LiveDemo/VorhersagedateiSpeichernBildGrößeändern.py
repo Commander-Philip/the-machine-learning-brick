0382193cff7e1fccfac8740ebee98fa432fa01f4
Philip Fritzen <Philip.Fritzen@web.de>
2025-09-10 21:24:04 +0200

    Live Demo refactored und Scores in den Dateinamen geschrieben

--- docs/LiveDemo/VorhersagedateiSpeichernBildGrößeändern.py
+++ docs/LiveDemo/VorhersagedateiSpeichernBildGrößeändern.py
@@ -1,7 +1,7 @@
 import ml
 import gc
 import sensor, image, time, os
-za=0
+circleIndex=0
 
 print("trying to load")
 print("Free mem before loading model:", gc.mem_free());
@@ -23,36 +23,55 @@ sensor.set_vflip(True)
 sensor.skip_frames(time = 2000)      # Warte auf Stabilisierung
 clock = time.clock()
 
+try:
+    os.rmdir("/sdcard/bilderopenmv")
+except OSError:
+    pass
+
+def savePictureWithTimestamp(filenamePrefix, filenameSuffix, imageToSave: image.Image):
+    # Zeitstempelformat YYYYMMDD_HHMMSS
+    formattedTimestamp = time.strftime("%H_%M_%S", time.localtime())
+    # Dateinamen zusammenbauen
+    filenameAus = f"/sdcard/bilderopenmv/{filenamePrefix}____{formattedTimestamp}__{filenameSuffix}.bmp"
+
+    try:
+        os.mkdir("/sdcard/bilderopenmv")
+        print("Ordner erstellt")
+    except OSError:
+        pass
+    imageToSave.save(filenameAus)
+    print(f"Saves picture: {filenameAus}")
+
 while(True):
     clock.tick()
     sensor.flush()
     print("Foto machen")
-    img = sensor.snapshot()
-    img2= img.copy()
+    currentSnapshot = sensor.snapshot()
+    currentSnapshotClone= currentSnapshot.copy()
     time.sleep_ms(2000)
     sensor.flush()
     #print(sensor.width(),sensor.height())
     print("neuesBild")
 
 
-    circles = img.find_circles(threshold=2500, x_margin=10, y_margin=10, r_margin=10,
+    circles = currentSnapshot.find_circles(threshold=2500, x_margin=10, y_margin=10, r_margin=10,
                                r_min=10, r_max=50, r_step=2)
 
     if not circles:
         continue
     print("Suche Kreis… Gefundene Kreise:", len(circles))
-    za=0
+    circleIndex = 0
     # 2) Sobald circles nicht leer ist, springt das Programm hierher:
     print("Kreis gefunden! Zeichne ein.")
-    orig_width = img.width()
-    orig_height = img.height()
+    orig_width = currentSnapshot.width()
+    orig_height = currentSnapshot.height()
     for c in circles:
         #time.sleep_ms(1000)
-        za = za + 1
+        circleIndex = circleIndex + 1
         #print("Kreis ", za)
         x, y, r = c.x(), c.y(), c.r()
         # Kreis rot einzeichnen
-        img.draw_circle(x, y, r, color=(0, 255, 0))
+        currentSnapshot.draw_circle(x, y, r, color=(0, 255, 0))
         #print(r)
         # ROI berechnen
         roi = (x - 50, y - 50, 100, 100)
@@ -63,7 +82,7 @@ while(True):
             roi[1] + roi[3] > orig_height):
             continue  # Ausschnitt außerhalb, überspringen
         # 4.1 Crop & Array erstellen
-        small = img2.copy(roi=roi)
+        small = currentSnapshotClone.copy(roi=roi)
 
         dst = image.Image(48, 48, sensor.RGB565)
 
@@ -78,20 +97,11 @@ while(True):
         try:
             output = model.predict([image_array])
             scores = output[0][0][0][0]
-            print("Klassifikation:", scores)
-            tm = time.localtime()
-            # Format YYYYMMDD_HHMMSS
-            timestr = "{:02d}{:02d}".format(
-                    tm[4], tm[5]
-                )
-            # 2) Zähler mit führenden Nullen (drei Stellen)
-            counter = "{:03d}".format(za)
-            # 3) Dateinamen zusammenbauen
-            #filename = f"/sdcard/bilderopenmv/roi_{timestr}_{counter}.bmp"
-            filenameAus = f"/sdcard/bilderopenmv/Aus_{timestr}_{counter}.bmp"
-            print (filenameAus)
-            #small.save(filename)
-            dst.save(filenameAus)
+
+            filenamePrefix = f"Schild_{scores[1]*100:03.1f}____keinSchild_{scores[0]*100:03.1f}"
+            filenameSuffix = f"{circleIndex:03d}"
+
+            savePictureWithTimestamp(filenamePrefix, filenameSuffix, dst)
 
         except Exception as e:
             print(e)
